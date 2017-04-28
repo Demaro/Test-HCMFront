2235f12967fe42affcd347a984b70d6f08b4d619
demaro <demaromail@gmail.com>
2017-04-28 01:39:17 -0300

    initial_2

--- Test/settings.py
+++ Test/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'apps.sala',
     'apps.reserva',
 
 ]
@@ -51,7 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    
+
 ]
 
 ROOT_URLCONF = 'Test.urls'
