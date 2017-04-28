06c7584698e622c001faa383b46d96553ed82c66
demaro <demaromail@gmail.com>
2017-04-28 03:21:23 -0300

    initial_2

--- apps/reserva/admin.py
+++ apps/reserva/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Reservacion, Sala
+from .models import Reservacion, Sala, Cant_personas, Cant_insumos, Insumo
 
 
 
@@ -15,8 +15,22 @@ class SalaAdmin(admin.ModelAdmin):
 	search_fields = ( 'capacidad__cantidad',)
 	list_editable = ('nombre', 'ubicacion', 'horario_disp', 'estado', 'capacidad',)
 
+class InsumoAdmin(admin.ModelAdmin):
+	list_display = ('id', 'nombre', 'descripcion', 'stock',)
+	list_filter  = ('nombre',)
+	list_editable = ('nombre', 'descripcion', 'stock',)
+
+class Cant_personasAdmin(admin.ModelAdmin):
+	list_display = ('id', 'cantidad',)
+	list_editable = ('cantidad',)
+
+class Cant_insumosAdmin(admin.ModelAdmin):
+	list_display = ('id', 'cantidad',)
+	list_editable = ('cantidad',)
+
 # Register your models here.
 admin.site.register(Reservacion, ReservacionAdmin)
-
+admin.site.register(Cant_personas, Cant_personasAdmin)
+admin.site.register(Insumo, InsumoAdmin)
 admin.site.register(Sala, SalaAdmin)
 
