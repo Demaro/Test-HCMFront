acf7172e811cfd5b477e2233570cc0ada7d3faaa
demaro <demaromail@gmail.com>
2017-04-28 02:11:34 -0300

    initial_2

--- Test/wsgi.py
+++ Test/wsgi.py
@@ -10,9 +10,9 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-#from whitenoise.django import DjangoWhiteNoise
+from django_static import Cling
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Test.settings")
 
 application = get_wsgi_application()
-#application = DjangoWhiteNoise(application)
+application = Cling(get_wsgi_application())
